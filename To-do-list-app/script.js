b4c6e396b3b7c2799d389da2a22e95445f6a84b7
beginner-coder-1281 <sangeetachoudhury63@gmail.com>
2026-04-16 10:25:14 +0530

    Add files via upload

--- To-do-list-app/script.js
+++ To-do-list-app/script.js
@@ -1,7 +1,9 @@
 const inputBox = document.getElementById("input-box");
 const taskList = document.getElementById("task-list");
 
+// function to add a task
 function addTask(){
+    // checking for empty tasks and giving a pop-up
     if(inputBox.value.trim() === ""){
         Swal.fire({
             toast: true,
@@ -15,6 +17,7 @@ function addTask(){
         return;
     }
     
+    // checking for long tasks and giving a pop-up
     if(inputBox.value.trim().length > 200){
         Swal.fire({
             toast: true,
@@ -29,16 +32,30 @@ function addTask(){
         return;
     }
     let li = document.createElement("li");
-    li.textContent = inputBox.value.trim();
+    
+    // converting user input into a safe value to avoid injecting scripts
+    let safeValue = inputBox.value.trim().replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    
+    // formatting the safeValue to allow for bold, italic, strikethrough, underline, emphasize and highlight
+    li.innerHTML = formatText(safeValue)
+    
+    // replacing my special syntax with a clickabl link
+    li.innerHTML = li.innerHTML
+    .replace(/~link address="([^"]+)"~ ([^~]+) ~\/link~/gi, '<a href="https://$1" target="_blank">$2</a>');
+    
     taskList.appendChild(li);
     
+    // creating delete button
     let span = document.createElement("span");
     span.textContent = "×";
+    span.id = "del"
     li.appendChild(span);
     inputBox.value = "";
     
+    // saving everything
     save();
     
+    // pop-up to let the user know the task has been added
     Swal.fire({
         toast: true,
         position: 'top-end',
@@ -50,12 +67,53 @@ function addTask(){
     });
 }
 
+// function to allow my special syntax and replace it with text formatting
+function formatText(text) {
+    return text.replace(
+        /~([biushe+]+)~ ([\s\S]+?) ~\/text-style~/gi,
+        (match, styleStr, content) => {
+
+            const styles = styleStr.split("+");
+            
+            // defining an open tag
+            let open = "";
+            let close = "";
+
+            for (let s of styles) {
+                if (s === "b") { open += "<b>"; close = "</b>" + close; }
+                if (s === "i") { open += "<i>"; close = "</i>" + close; }
+                if (s === "u") { open += "<u>"; close = "</u>" + close; }
+                if (s === "s") { open += "<s>"; close = "</s>" + close; }
+                if (s === "h") { open += "<mark>"; close = "</mark>" + close; }
+                if (s === "e") { open += "<em>"; close = "</em>" + close; }
+            }
+            /*  When a style is detected, it is replaced by its html tag and the closing and opening part is updated
+             
+            
+            if close = null
+            then a tag is detected, so for example close = "</b>" + close, or "</b>" + null
+            
+            then another tag is detecting, then close = "</i>" + close, or "</i>" + "</b>", and so on, maintaining the order of tags
+            
+            
+            and for opening we just add the opening tag
+            
+             */
+
+            return open + content + close;
+        }
+    );
+}
+
+// functions to save and load data
 function save(){
     localStorage.setItem("data", taskList.innerHTML)
 }
 function load(){
     taskList.innerHTML = localStorage.getItem("data") || "";
 }
+
+// function ti toggle the checked class if user clicks on a LI, if they click on a SPAN, asks for comfirmation before deleting task
 taskList.addEventListener("click", function(e){
     if(e.target.tagName == "LI"){
         e.target.classList.toggle("checked");
@@ -73,6 +131,7 @@ taskList.addEventListener("click", function(e){
                 e.target.parentElement.classList.add("fade");
                 setTimeout(function(){
                     e.target.parentElement.remove();
+                    save();
                 }, 500);
                 Swal.fire({
                     toast: true,
@@ -83,7 +142,6 @@ taskList.addEventListener("click", function(e){
                     timer: 2500,
                     timerProgressBar: true
                 });
-                save();
             }
         });
     }
