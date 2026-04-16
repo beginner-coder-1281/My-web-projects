b13a36441cb24e1bdab277acc9769add5593659c
beginner-coder-1281 <sangeetachoudhury63@gmail.com>
2026-04-16 10:25:38 +0530

    Add files via upload

--- Weather-App/script.js
+++ Weather-App/script.js
@@ -1,33 +1,40 @@
+// defining core variables
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = //Your Api Key here
+const apiKey = "86ecb9900888bf4aacb57bf51d95e6b7";
 
 weatherForm.addEventListener("submit", async event => {
     
     event.preventDefault();
     const city = cityInput.value;
     
+    // if input is given, get weather data and display it
     if(city){
         try{
             const weatherData = await getWeatherData(city);
             displayWeatherData(weatherData)
         }
+        // if error, display a error to the user and console it
         catch(error){
             console.error(error);
             displayError(error)
         }
     }
+    // if no input given, displaying a Error to the user
     else{
         displayError("Please enter a city");
     }
 });
 
+// function to get data from the api
 async function getWeatherData(city){
+    // url to get data from
     const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
     
     const response = await fetch(apiURL);
     
+    // error if reponse not ok
     if(!response.ok){
         throw new Error("Could not fetch weather data");
     }
@@ -40,30 +47,32 @@ function displayWeatherData(data){
            main: {temp, humidity}, 
            weather: [{description, id}]} = data;
     
-    
+    // displaying the card
     card.textContent = "";
     card.style.display = "flex";
     
+    // creating the elements
     cityDisplay = document.createElement("h1");
     tempDisplay = document.createElement("p");
     humidityDisplay = document.createElement("p");
     descDisplay = document.createElement("p");
     emojiDisplay = document.createElement("p");
     
+    // changing text content of the elements
     cityDisplay.textContent = city;
     tempDisplay.textContent = (Number(temp) - 273.15).toFixed(2) + "°C";
     humidityDisplay.textContent = `humidity: ${humidity}%`;
     descDisplay.textContent = description;
     emojiDisplay.textContent = getWeatherEmoji(id);
     
-    
+    // adding a class to each element
     cityDisplay.classList.add("cityDisplay");
     tempDisplay.classList.add("tempDisplay");
     humidityDisplay.classList.add("humidityDisplay");
     descDisplay.classList.add("descDisplay");
     emojiDisplay.classList.add("emojiDisplay");
     
-    
+    // append each element to the card
     card.appendChild(cityDisplay);
     card.appendChild(tempDisplay);
     card.appendChild(humidityDisplay);
@@ -71,6 +80,7 @@ function displayWeatherData(data){
     card.appendChild(emojiDisplay);
 }
 
+// getting weather emoji based on the weatherID
 function getWeatherEmoji(id){
     switch (true) {
        case (id >= 200 && id < 300): 
@@ -89,14 +99,17 @@ function getWeatherEmoji(id){
            return "☁️";
        default:
            return "❓";
-}
+    }
 }
 
+// function to display error
 function displayError(message){
+    // creating error element
     const errorDisplay = document.createElement("p");
     errorDisplay.textContent = message;
     errorDisplay.classList.add("error");
     
+    // emptying card and appending error element
     card.textContent = "";
     card.style.display = "flex";
     card.appendChild(errorDisplay);
